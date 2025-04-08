09280f48650d8ac60295526b4e745cbfa48b2255
Aman Sunesh <157351006+Aman-Sunesh@users.noreply.github.com>
2025-04-08 14:08:53 +0000

    Initial commit of all project files

--- problem1.cpp
+++ problem1.cpp
@@ -1,84 +1,124 @@
-#pragma once
+#include <iostream>
 
-#include "payload.h"
+using namespace std;
 
-struct Node
+class Node 
 {
-    Payload data;
+public:
+    int value;
     Node* next;
-
-    Node(const Payload& data)
-    {
-        this->data = data;
-        this->next = nullptr;
-    }
+    Node(int x) : value(x), next(nullptr) {}
 };
 
-struct List
+// Rearranges the list such that even numbers come before odd numbers,
+// preserving the relative order within both groups.
+Node* rearrangeEvenOdd(Node* head) 
 {
-    Node* head;
+    Node* evenStart = nullptr; // Head of even sublist
+    Node* evenEnd = nullptr;   // Tail of even sublist
+    Node* oddStart = nullptr;  // Head of odd sublist
+    Node* oddEnd = nullptr;    // Tail of odd sublist
+    Node* current;
 
-    List();
-    void push_front(const Payload& x);
-    void pop_front();
-    void push_back(const Payload& x);
-    void pop_back();
-    bool empty() const;
-    Payload front() const;
-    Payload back() const;
-    Payload& operator[](int i);
-    Payload& operator[](int i) const;
-    int size() const;
-    int capacity() const;
-    void print() const;
-    ~List();
-
-    struct ListIterator
+    // Traverse the list and partition nodes into even and odd lists
+    for (current = head; current != nullptr; current = current->next)
     {
-        Node* node;
+        int value = current->value;
 
-        ListIterator(Node* node)
+        if (value % 2 == 0)  // Even node
         {
-            this->node = node;
-        }
-
-        ListIterator& operator ++()
-        {
-            if (node != nullptr)
+            if (evenStart != nullptr)
             {
-                node = node->next;
+                evenEnd->next = current;
+                evenEnd = evenEnd->next;
             }
-            
-            return *this;
-        }
-
-        bool operator !=(const ListIterator& node2) const
-        {
-            if (this->node != node2.node)
-            {
-                return true;
-            }
-
             else
             {
-                return false;
+                evenStart = current;
+                evenEnd = evenStart;
             }
         }
-
-        Payload& operator *()
+        else  // Odd node
         {
-            return node->data;
+            if (oddStart != nullptr)
+            {
+                oddEnd->next = current;
+                oddEnd = oddEnd->next;
+            }
+            else 
+            {
+                oddStart = current;
+                oddEnd = oddStart;
+            }
         }
-        
-    };
-
-    ListIterator begin()
-    {
-        return ListIterator(head);
     }
 
-    ListIterator end()
+    // If either even or odd list is empty, return original head
+    if (!oddStart || !evenStart)
     {
-        return ListIterator(nullptr);
+        return head;
     }
-};
+     
+    // Combine even and odd lists
+    evenEnd->next = oddStart;
+    oddEnd->next = nullptr;
+     
+    // Update head to the start of the even list
+    head = evenStart;
+    return head;
+}
+
+
+void printList(Node* head) 
+{
+    Node* current = head;
+
+    while (current != nullptr) 
+    {
+        cout << current->value << "->";
+        current = current->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+void push_back(Node*& head, int val) 
+{
+    Node* newNode = new Node(val);
+
+    if (!head) 
+    {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+
+    while (temp->next != nullptr) 
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
+
+int main() {
+    Node* head = nullptr;
+
+    // Example input: 17->15->8->12->10->5->4->1->7->6->NULL
+    int values[] = {17, 15, 8, 12, 10, 5, 4, 1, 7, 6};
+    for (int val : values) 
+    {
+        push_back(head, val);
+    }
+
+    cout << "Original List:" << endl;
+    printList(head);
+
+    head = rearrangeEvenOdd(head);
+
+    cout << "Rearranged List:" << endl;
+    printList(head);
+
+    return 0;
+}
